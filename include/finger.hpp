cff660a6b69fcdf92df7fa326b5bb7e6171358f8
Francois Qian <fqian@Francoiss-MacBook-Pro.local>
2024-08-02 06:52:19 +0100

    finished finger and key

--- include/finger.hpp
+++ include/finger.hpp
@@ -1,9 +1,11 @@
 #pragma once
+#include "iostream"
+#include <cmath>
 
 class Finger
 {
-    int fingerId;
-    double score;
+public:
+    const int fingerId;
     const double dexterity;
     const double homeX;
     const double homeY;
@@ -12,8 +14,8 @@ class Finger
 
     Finger(int fingerId, double dexterity, double homeX, double homeY);
 
-    static inline double distance(const double& posX, const double& posY);
-    double moveToPos(const double& x, const double& y);
-    void returnToHome();
-    void print() const;
+    inline double distanceTo(const double& x, const double& y);
+    inline void moveToPos(const double& x, const double& y);
+    inline void returnToHome();
+    inline void print() const;
 };
--- src/finger.cpp
+++ src/finger.cpp
@@ -1,11 +1,29 @@
 #include "iostream"
+#include "finger.hpp"
 #include <cmath>
-#include "include/finger.hpp"
 
 Finger::Finger(int fingerId, double dexterity, double homeX, double homeY)
-        : fingerId(fingerId), dexterity(dexterity), homeX(homeX), homeY(homeY), posX(homeX), posY(homeY) {}
+        : fingerId(fingerId), dexterity(dexterity), homeX(homeX), homeY(homeY), posX(homeX), posY(homeY) {};
 
-static inline double distance(const double& x, const double& y)
+inline double Finger::distanceTo(const double& x, const double& y)
 {
-    return sqrt((posX - x)**2 + (posY - y)**2);
+    return sqrt((posX - x)*(posX - x) + (posY - y)*(posY - y));
+}
+
+inline void Finger::moveToPos(const double& x, const double& y)
+{
+    posX = x;
+    posY = y;
+}
+
+inline void Finger::returnToHome()
+{
+    posX = homeX;
+    posY = homeY;
+}
+
+inline void Finger::print() const
+{
+    std::cout << "Id: " << fingerId << "\nDexterity: " << dexterity << "\nHome Pos: " << homeX 
+              << ", " << homeY << "\nCurrent Pos: " << posX << ", " << posY << "\n";
 }

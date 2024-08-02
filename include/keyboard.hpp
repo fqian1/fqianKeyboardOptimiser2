c37d6bde8015cdaed56d786266b13f49098c2af0
Francois Qian <fqian@Francoiss-MacBook-Pro.local>
2024-08-02 22:28:17 +0100

    m

--- include/keyboard.hpp
+++ include/keyboard.hpp
@@ -1,2 +1,15 @@
 #pragma once
 
+#include <unordered_map>
+#include "key.hpp"
+#include "finger.hpp"
+
+class Keyboard
+{
+public:
+    std::wstring_view charLayout;
+    std::vector<Key> keys;
+    std::vector<Finger> fingers;
+    std::unordered_map<
+}
+
